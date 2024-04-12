e81fead96ceafff09f5f9c7de520ad6d353970e5
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-04-12 00:58:25 +0000

    update git link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-gray-800 text-white p-4">
     <div class="container mx-auto text-center">
-      <p class="text-sm">Open source on <a href="https://github.com/jonahkleinman/EDPoints" target="_blank" class="text-blue-400 hover:text-blue-600 transition duration-300 ease-in-out" rel="noopener noreferrer">GitHub</a></p>
+      <p class="text-sm">Open source on <a href="https://github.com/jonahkleinman/PointSystem" target="_blank" class="text-blue-400 hover:text-blue-600 transition duration-300 ease-in-out" rel="noopener noreferrer">GitHub</a></p>
     </div>
   </footer>
 </template>
