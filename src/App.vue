606658ca36c322c9d0bec609b299263691d93be7
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-04-15 21:08:33 -0400

    add admin page to header

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,47 @@
 <style>
-  @import 'tailwindcss/base';
-  @import 'tailwindcss/components';
-  @import 'tailwindcss/utilities';
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
 </style>
+
 <script setup>
-  import Header from './components/Header.vue'
-  import Footer from './components/Footer.vue'
-  import { onMounted, ref } from 'vue'
-  import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import { onMounted, ref } from 'vue'
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
+import router from './router'
 
-  const isLoggedIn = ref(false)
+const isLoggedIn = ref(false)
+const isAdmin = ref(false)
 
-  let auth;
-  onMounted(() => {
-    auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        isLoggedIn.value = true;
-      } else {
-        isLoggedIn.value = false;
-      }
-    })
+let auth;
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true;
+      isAdmin.value = user.uid === 'DB9ljAWIxZUaHSKoVL8jbYao4u32';
+    } else {
+      isLoggedIn.value = false;
+      isAdmin.value = false;
+    }
   })
+})
 
-  const handleSignOut = () => {
-    signOut(auth).then(() => {
-      router.push('/')
-    })
-  }
-  defineExpose({ handleSignOut })
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    isLoggedIn.value = false;
+    isAdmin.value = false;
+    router.push('/')
+  })
+}
+defineExpose({ handleSignOut })
 </script>
 
 
 <template>
   <div id="app" class="min-h-screen bg-gray-800 text-white">
-    <Header :isLoggedIn="isLoggedIn" :handleSignOut="handleSignOut" />
+    <Header :isLoggedIn="isLoggedIn" :is-admin="isAdmin" :handleSignOut="handleSignOut" />
     <router-view/>
     <Footer />
   </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,7 @@
         <router-link to="/" class="hover:text-blue-400 transition duration-300 ease-in-out">Home</router-link>
         <router-link to="/add" v-if='isLoggedIn' class="hover:text-blue-400 transition duration-300 ease-in-out">Add Points</router-link>
         <router-link to="/login" v-if='!isLoggedIn' class="hover:text-blue-400 transition duration-300 ease-in-out">Login</router-link>
+        <router-link to="/admin" v-if="isAdmin" class="hover:text-blue-400 transition duration-300 ease-in-out">Admin</router-link>
       </div>
       <button @click="handleSignOut" v-if='isLoggedIn' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">Sign Out</button>
     </nav> 
@@ -14,6 +15,6 @@
 <script>
   export default {
     name: 'Header',
-    props: ['isLoggedIn', 'handleSignOut']
+    props: ['isLoggedIn', 'isAdmin', 'handleSignOut']
   }
 </script>
